4b0ad339afe10a990b888e53664d49f8460c02d8
Jason Barnes <deathheart14@yahoo.com>
2017-09-09 00:53:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
 
 
   if (string.toLowerCase === string){
-  console.log('I can\'t hear you!')}
+  return string('I can\'t hear you!')}
   else if(string.toUpperCase === string){
     console.log('YES INDEED!');
   }
