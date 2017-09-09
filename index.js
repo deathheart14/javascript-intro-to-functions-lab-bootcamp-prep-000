029d252f402676d3f3dffe400f90812fe9b58441
Jason Barnes <deathheart14@yahoo.com>
2017-09-09 00:51:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 
-  string.toUpperCase === string
-  if (string.toUpperCase === string){
+
+  if (string.toLowerCase === string){
   console.log('I can\'t hear you!')}
+  else if(string.toUpperCase === string){
+    console.log('YES INDEED!');
+  }
+  else if(string.toLowerCase === string | string.toUpperCase === string) {
+    console.log('I love you, too');
+  }
 }
